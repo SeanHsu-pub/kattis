6790d4dbe815a695fc57c97d0507486eb64c5479
SeanHsu-pub <shobu.pub@gmail.com>
2022-06-11 02:34:33 +0800

    SeanHsu202206110234

---
+++ CompetitiveProgramming01/Solving_for_Carrots.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+	int a, b;
+	cin >> a >> b;
+	const int ca=a;
+	string c[ca]={};
+	for(int i=0; i<a; i++){
+		cin >> c[i];
+	}
+	cout << b;
+	return 0;
+}
