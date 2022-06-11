1dd51a869b369b131d73a159674e27b2ee47014e
SeanHsu-pub <shobu.pub@gmail.com>
2022-06-11 16:33:55 +0800

    SeanHsu202206111633

--- CompetitiveProgramming01/FYI.cpp
+++ CompetitiveProgramming01/FYI.cpp
@@ -1,1 +1,21 @@
-#
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+	char c[7]={};
+	for(int i=0; i<7; i++){
+		cin >> c[i];
+	}
+	bool a=true;
+	if(c[0]=='5'){
+		if(c[1]=='5'){
+			if(c[2]=='5'){
+				cout << 1;
+				a=false;
+			}
+		}
+	}
+	if(a){
+		cout << 0;
+	}
+	return 0;
+}
