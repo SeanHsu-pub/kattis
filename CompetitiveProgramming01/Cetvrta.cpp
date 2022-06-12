8d78e1c7a2848a6366b04e9ab7ea756052109f8a
SeanHsu-pub <shobu.pub@gmail.com>
2022-06-12 23:51:33 +0800

    SeanHsu06122351

---
+++ CompetitiveProgramming01/Cetvrta.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+	int x[3]={};
+	int y[3]={};
+	cin >> x[0] >> y[0] >> x[1] >> y[1] >> x[2] >> y[2];
+	if((x[0]!=x[1])&&(y[0]!=y[1])){
+		cout << x[0]+x[1]-x[2] << " " << y[0]+y[1]-y[2];
+	}
+	if((x[2]!=x[1])&&(y[2]!=y[1])){
+		cout << x[2]+x[1]-x[0] << " " << y[2]+y[1]-y[0];
+	}
+	if((x[0]!=x[2])&&(y[0]!=y[2])){
+		cout << x[0]+x[2]-x[1] << " " << y[0]+y[2]-y[1];
+	}
+	return 0;
+}
