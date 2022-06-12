eeec4d8e9d90d2b8c8a28e755cd0933efe8ea385
SeanHsu-pub <shobu.pub@gmail.com>
2022-06-13 00:06:35 +0800

    SeanHsu202206130006

---
+++ CompetitiveProgramming01/Bijele.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+	int a0, a1, a2, a3, a4, a5;
+	cin >> a0 >> a1 >> a2 >> a3 >> a4 >> a5;
+	cout << 1-a0 << " " << 1-a1 << " " << 2-a2 << " " << 2-a3 << " " << 2-a4 << " " << 8-a5;
+	return 0;
+}
